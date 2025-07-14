cb8beba3ac25874c18596c5512ede7e3120f70d6
sinezplered <sinezplered@gmail.com>
2025-07-15 04:06:04 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Aril Noah';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
-export const url = 'https://bokeparilnoah.pages.dev';
-export const terbit = '2025-01-28T00:01:03Z';
